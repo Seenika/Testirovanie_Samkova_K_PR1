45bd1e58e470d5a7df28094dfbeffc269a5c7736
Seenika <114129910+Seenika@users.noreply.github.com>
2022-11-21 10:31:11 +1000

    Add files via upload

---
+++ HelloWorldTests_SK/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldTests_SK")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldTests_SK")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64e8b6b9-a419-4368-b9c3-235604759894")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HelloWorldTests_SK/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace HelloWorldTests_SK
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Testirovanie_Samkova_K_PR1.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
